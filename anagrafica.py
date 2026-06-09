39d78dc675ccc037358fcf7938f7422fe1fec516
Simone <simonebarondi@gmail.com>
2026-06-09 18:05:07 +0200

    Aggiunto P6059C

--- anagrafica.py
+++ anagrafica.py
@@ -62,6 +62,7 @@ prodotti = [
     Prodotto('D3336C', 'salicilato', riga=71, analisi=(0, 6, 7, 3, 10, 13, 11, 17, 12, 1, 2, 9)),
     Prodotto('P6072C', 'salicilato', riga=72, analisi=(0, 6, 7, 3, 2, 13, 16, 17, 12, 10, 1, 15, 9, 18)),
     Prodotto('P6097C', 'salicilato', riga=72, analisi=(0, 6, 7, 3, 2, 13, 17, 12, 10, 1, 15, 5, 9, 18)),
+    Prodotto('P6059C', 'salicilato', riga=73, analisi=(0, 6, 7, 3, 2, 14, 13, 16, 17, 12, 10, 1, 15, 9, 18)),
     Prodotto('P6571C', 'salicilato', riga=72, analisi=(0, 6, 7, 3, 2, 13, 16, 17, 12, 4, 1, 5, 9)),
     Prodotto('P6052C', 'salicilato', riga=71, analisi=(0, 6, 7, 3, 10, 13, 17, 12, 1, 16, 2, 14, 9)),
     Prodotto('P6660C', 'salicilato', riga=73, analisi=(0, 6, 7, 3, 13, 17, 12, 10, 16, 14, 1, 9)),
